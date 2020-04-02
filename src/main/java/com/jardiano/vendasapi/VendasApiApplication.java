54e2b64e5b670a17902a4b143da86d18bcaafec0
jardianoalmeida <61389908+jardianoalmeida@users.noreply.github.com>
2020-04-02 08:57:37 -0300

    Configurações do bean

--- src/main/java/com/jardiano/vendasapi/VendasApiApplication.java
+++ src/main/java/com/jardiano/vendasapi/VendasApiApplication.java
@@ -1,10 +1,24 @@
 package com.jardiano.vendasapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApiApplication {
+
+    @Autowired
+    @Qualifier("applicationName")
+    private String applicationName;
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return applicationName;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApiApplication.class, args);
