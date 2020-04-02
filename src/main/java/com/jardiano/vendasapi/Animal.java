d08c83308ab4320115166a31e4201ca5eace0eb8
jardianoalmeida <61389908+jardianoalmeida@users.noreply.github.com>
2020-04-02 09:20:07 -0300

    Criando annotations customizadas de configuração

---
+++ src/main/java/com/jardiano/vendasapi/Animal.java
@@ -0,0 +1,5 @@
+package com.jardiano.vendasapi;
+
+public interface Animal {
+    void fazerBarulho();
+}
--- src/main/java/com/jardiano/vendasapi/VendasApiApplication.java
+++ src/main/java/com/jardiano/vendasapi/VendasApiApplication.java
@@ -2,8 +2,11 @@ package com.jardiano.vendasapi;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasApiApplication {
 
-    @Autowired
-    @Qualifier("applicationName")
+    @Value("${application.name}")
     private String applicationName;
+
+    @Cachorro
+    private Animal animal;
+
+    @Bean(name = "executarAnimal")
+    public CommandLineRunner executar(){
+        return args -> {
+            this.animal.fazerBarulho();
+        };
+    }
 
     @GetMapping("/hello")
     public String helloWorld(){
@@ -23,5 +35,4 @@ public class VendasApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(VendasApiApplication.class, args);
     }
-
 }
