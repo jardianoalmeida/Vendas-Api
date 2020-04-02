f0b7288bf698f5c5e4d6501ee614a1ef981ea1e0
jardianoalmeida <61389908+jardianoalmeida@users.noreply.github.com>
2020-04-02 09:07:10 -0300

    Injeção de Dependências

---
+++ src/main/java/com/jardiano/vendasapi/model/Cliente.java
@@ -0,0 +1,13 @@
+package com.jardiano.vendasapi.model;
+
+public class Cliente {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
