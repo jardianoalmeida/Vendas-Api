f4696dade3c63e181b0aee414e0ec890a13b23eb
jardianoalmeida <61389908+jardianoalmeida@users.noreply.github.com>
2020-04-03 09:01:28 -0300

    Utilizando @query

--- src/main/java/com/jardiano/vendasapi/VendasApiApplication.java
+++ src/main/java/com/jardiano/vendasapi/VendasApiApplication.java
@@ -12,39 +12,18 @@ import java.util.List;
 
 @SpringBootApplication
 public class VendasApiApplication {
+
     @Bean
     public CommandLineRunner init(@Autowired Clientes clientes){
         return args -> {
             System.out.println("Salvando clientes");
-            clientes.salvar(new Cliente("Dougllas"));
-            clientes.salvar(new Cliente("Outro Cliente"));
+            clientes.save(new Cliente("Fulano"));
+            clientes.save(new Cliente("Outro Cliente"));
 
-            List<Cliente> todosClientes = clientes.obterTodos();
-            todosClientes.forEach(System.out::println);
+            List<Cliente> result = clientes.encontrarPorNome("Dougllas");
+            result.forEach(System.out::println);
 
-            System.out.println("Atualizando clientes");
-            todosClientes.forEach(c -> {
-                c.setNome(c.getNome() + " atualizado.");
-                clientes.atualizar(c);
-            });
 
-            todosClientes = clientes.obterTodos();
-            todosClientes.forEach(System.out::println);
-
-            System.out.println("Buscando clientes");
-            clientes.buscarPorNome("Cli").forEach(System.out::println);
-
-            System.out.println("deletando clientes");
-            clientes.obterTodos().forEach(c -> {
-                clientes.deletar(c);
-            });
-
-            todosClientes = clientes.obterTodos();
-            if(todosClientes.isEmpty()){
-                System.out.println("Nenhum cliente encontrado.");
-            }else{
-                todosClientes.forEach(System.out::println);
-            }
         };
     }
 
--- src/main/java/com/jardiano/vendasapi/repositorio/Clientes.java
+++ src/main/java/com/jardiano/vendasapi/repositorio/Clientes.java
@@ -1,59 +1,22 @@
 package com.jardiano.vendasapi.repositorio;
 
 import com.jardiano.vendasapi.entity.Cliente;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
 import java.util.List;
 
-@Repository
-public class Clientes {
+public interface Clientes extends JpaRepository<Cliente, Integer > {
 
-    @Autowired
-    private EntityManager entityManager;
+    @Query(value = " select * from cliente c where c.nome like '%:nome%' ", nativeQuery = true)
+    List<Cliente> encontrarPorNome( @Param("nome") String nome );
 
-    @Transactional
-    public Cliente salvar(Cliente cliente){
-        entityManager.persist(cliente);
-        return cliente;
-    }
+    @Query(" delete from Cliente c where c.nome =:nome ")
+    @Modifying
+    void deleteByNome(String nome);
 
-    @Transactional
-    public Cliente atualizar(Cliente cliente){
-        entityManager.merge(cliente);
-        return cliente;
-    }
-
-    @Transactional
-    public void deletar(Cliente cliente){
-        if(!entityManager.contains(cliente)){
-            cliente = entityManager.merge(cliente);
-        }
-        entityManager.remove(cliente);
-    }
-
-    @Transactional
-    public void deletar(Integer id){
-        Cliente cliente = entityManager.find( Cliente.class, id );
-        deletar(cliente);
-    }
-
-    @Transactional(readOnly = true)
-    public List<Cliente> buscarPorNome(String nome){
-       String jpql = " select c from Cliente c where c.nome like :nome ";
-       TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
-       query.setParameter("nome", "%" + nome +"%");
-       return query.getResultList();
-    }
-
-    @Transactional(readOnly = true)
-    public List<Cliente> obterTodos(){
-        return entityManager
-                .createQuery("from Cliente", Cliente.class)
-                .getResultList();
-    }
+    boolean existsByNome(String nome);
 
 }
